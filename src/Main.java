276f41d789e7c5f75e5720f6c602d7943f6c0420
gilles.reist <gilles.reist@le-campus-numerique.fr>
2023-08-03 16:28:53 +0200

    ajout du plateau

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the dungeon of Khazad Dum!");
+        boolean quit = false;
         Menu menu = new Menu();
         CreateCharacter character = menu.run();
-        Play play = new Play(character);
-        play.tilTheEnd();
-
-
+        while (!quit) {
+            Play play = new Play(character);
+            quit = play.tilTheEnd();
+        }
+        System.out.println("Goodbye.");
     }
 }
